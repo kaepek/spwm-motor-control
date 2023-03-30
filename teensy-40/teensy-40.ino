a8fc866447a54191311c0818bd2a5360b5524000
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2023-03-30 19:43:00 +0100

    moving away from pots to pure digital usb

--- teensy-40/teensy-40.ino
+++ teensy-40/teensy-40.ino
@@ -2,8 +2,6 @@
 #define PIN_L6234_SPWM_PHASE_B 0
 #define PIN_L6234_SPWM_PHASE_C 7
 #define PIN_L6234_SPWM_EN 8
-#define PIN_SPEED_POT A0 //14
-#define PIN_TORQUE_POT A1 //15
 #define PWM_FREQUENCY 36000
 
 int ELECTRICAL_DEG_PHASE_A = -1;
@@ -13,8 +11,9 @@ int SPWM_DUTY_PHASE_A = 0;
 int SPWM_DUTY_PHASE_B = 0;
 int SPWM_DUTY_PHASE_C = 0;
 
-int SPEED_POT_VALUE = 0;  // range 0 1023
-int TORQUE_POT_VALUE = 0; // range 0 1023 (need to map to range 0 -> 1)
+volatile float TORQUE_VALUE = 0.0;
+volatile unsigned int DELAY_VALUE = 500;
+volatile unsigned int DIRECTION_VALUE = 0;
 
 long int t1 = millis(); // loop timing variable
 
@@ -29,10 +28,6 @@ void setup()
   pinMode(PIN_L6234_SPWM_EN, OUTPUT);
   digitalWrite(PIN_L6234_SPWM_EN, HIGH);
 
-  // setup input pins
-  pinMode(PIN_SPEED_POT, INPUT);
-  pinMode(PIN_TORQUE_POT, INPUT);
-
   analogWriteRes(8);
 
   analogWriteFrequency(PIN_L6234_SPWM_PHASE_A, PWM_FREQUENCY);
@@ -40,6 +35,12 @@ void setup()
   analogWriteFrequency(PIN_L6234_SPWM_PHASE_C, PWM_FREQUENCY);
 
   increment_rotation();
+
+  // delay serial read as too early and it gets junk noise data
+  while (!Serial.available())
+  {
+    delay(100);
+  }
 }
 
 void increment_rotation()
@@ -78,14 +79,6 @@ void update_spwm_duties()
   long int oldTime = t1;
   t1 = millis();
 
-  // read pot values
-  SPEED_POT_VALUE = analogRead(PIN_SPEED_POT);
-  TORQUE_POT_VALUE = analogRead(PIN_TORQUE_POT);
-
-  // map and clamp torque value
-  float TORQUE_VALUE = map_int_to_float(TORQUE_POT_VALUE, 0, 1023, 0.0, 1.0);
-  TORQUE_VALUE = min(TORQUE_VALUE, 0.7);
-
   // calculate SPWM duties
   SPWM_DUTY_PHASE_A = (int)(spwm_duty_calculator(ELECTRICAL_DEG_PHASE_A) * TORQUE_VALUE);
   SPWM_DUTY_PHASE_B = (int)(spwm_duty_calculator(ELECTRICAL_DEG_PHASE_B) * TORQUE_VALUE);
@@ -97,11 +90,13 @@ void update_spwm_duties()
   analogWrite(PIN_L6234_SPWM_PHASE_C, SPWM_DUTY_PHASE_C);
 
   // diagnostic prints
-  Serial.print(SPEED_POT_VALUE);
+  Serial.print(DIRECTION_VALUE);
+  Serial.print("\t");
+  Serial.print(DELAY_VALUE);
   Serial.print("\t");
   Serial.print(TORQUE_VALUE);
   Serial.print("\t");
-  Serial.print(t1-oldTime);
+  Serial.print(t1 - oldTime);
 
   Serial.print("\t");
   Serial.print(SPWM_DUTY_PHASE_A);
@@ -109,10 +104,41 @@ void update_spwm_duties()
   Serial.print(SPWM_DUTY_PHASE_B);
   Serial.print("\t");
   Serial.print(SPWM_DUTY_PHASE_C);
-  
+
   Serial.print("\n");
 
-  delay(SPEED_POT_VALUE / 15);
+  // delay(ms)
+  // delay(SPEED_POT_VALUE / 30);
+  delayMicroseconds(DELAY_VALUE);
+}
+
+// we read 2 bytes in total
+const int SIZE_OF_PROFILE = 4;
+
+// buffer to store the thrust/direction profile from the serial stream
+char HOST_PROFILE_BUFFER[SIZE_OF_PROFILE] = {0, 0, 0, 0};
+byte HOST_PROFILE_BUFFER_CTR = 0;
+
+bool readHostControlProfile()
+{
+  bool proccessedAFullProfile = false;
+  cli(); // no interrupt
+  while (Serial.available())
+  {
+    HOST_PROFILE_BUFFER[HOST_PROFILE_BUFFER_CTR] = Serial.read(); // read byte from usb
+    HOST_PROFILE_BUFFER_CTR++;                                    // in buffer
+    if (HOST_PROFILE_BUFFER_CTR % SIZE_OF_PROFILE == 0)
+    { // when we have the right number of bytes for the whole input profile
+      TORQUE_VALUE = min((float)HOST_PROFILE_BUFFER[0] / (float)255, 0.7);
+      DELAY_VALUE = (HOST_PROFILE_BUFFER[2] << 8) | HOST_PROFILE_BUFFER[1];
+      // extract direction from buffer (0 is cw 1 is ccw)
+      DIRECTION_VALUE = HOST_PROFILE_BUFFER[3];
+      proccessedAFullProfile = true; // indicate we have processed a full profile
+    }
+    HOST_PROFILE_BUFFER_CTR %= SIZE_OF_PROFILE; // reset buffer ctr for a new profile
+  }
+  sei(); // interrupt
+  return proccessedAFullProfile;
 }
 
 void loop()
@@ -122,4 +148,7 @@ void loop()
 
   // update pwm signal
   update_spwm_duties();
+
+  // take user input
+  readHostControlProfile();
 }
