e3f3dc879c4f3541d53e59323575fde8a943e6d0
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2023-08-21 15:35:29 +0100

    less logging

--- speed-control/closed-loop/AS5147P/teensy40/esc/esc.ino
+++ speed-control/closed-loop/AS5147P/teensy40/esc/esc.ino
@@ -6,7 +6,7 @@ using namespace TeensyTimerTool;
 PeriodicTimer logging_timer(GPT2);
 
 // Logging timer
-const std::size_t LOGGING_MICROS = 10000; // 40000
+const std::size_t LOGGING_MICROS = 20000; // 40000
 
 #ifndef DISABLE_LOGGING_CTRS
 #define DISABLE_LOGGING_CTRS false
