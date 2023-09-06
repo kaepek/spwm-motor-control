98e4b0ca94c6a542cb8b213e0dc04cac25494d12
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2023-09-06 11:29:24 +0100

    working on segmentation

--- speed-control/closed-loop/AS5147P/teensy40/programs/step-change-detector.js
+++ speed-control/closed-loop/AS5147P/teensy40/programs/step-change-detector.js
@@ -14,7 +14,7 @@ const parse_options = {
         },
         output_data_file: {
             type: "string",
-            short: "f"
+            short: "o"
         }
     }
 };
@@ -50,6 +50,14 @@ const values = parsed_options.values;
 // check if files exists
 const cwd = process.cwd();
 
+const full_output_data_path = `${cwd}/${values["output_data_file"]}`;
+const output_data_path_exists = fs.existsSync(full_output_data_path);
+if (output_data_path_exists) {
+    console.error(`StepChangeDetector: output_data_file ${full_input_config_path} exists already.`);
+    process.exit(1);
+}
+
+
 const full_input_config_path = `${cwd}/${values["input_config_file"]}`;
 const input_config_exists = fs.existsSync(full_input_config_path);
 if (!input_config_exists) {
@@ -76,7 +84,7 @@ if (!input_data_file_exists) {
 
 let file_input_data_str = null;
 try {
-    file_input_data_str = fs.readFileSync(full_input_data_path);
+    file_input_data_str = fs.readFileSync(full_input_data_path).toString();
 }
 catch (e) {
     console.error(`StepChangeDetector: error parsing input_data_file ${full_input_data_path} ${e.stack}`);
@@ -97,28 +105,43 @@ const segments = [];
 
 let e2v_std_min = Number.POSITIVE_INFINITY;
 let first_e2v_value_for_transition = Number.POSITIVE_INFINITY;
+let tmp_buffer = [];
+let index = 0;
+let min_index = -1;
+let check_for_escape = false;
+let highest_velocity = -1;
 
 data_lines.forEach((line) => {
     const line_data = parser.deserialise(line);
     const thrust = line_data["com_thrust_percentage"];
+    const velocity = line_data["kalman_velocity"];
+    if (velocity > highest_velocity) highest_velocity = velocity;
     if (com_thrust === -1) { // first stage (assume 0)
+        console.log("here steady start");
         // create first segment
         segments.push({type:"steady", data:[line_data]});
         com_thrust = thrust;
     }
     else if (com_thrust != thrust) { // we have a signal indicating the start of the transition
+        console.log("here transition start", thrust);
         // create transition segment
-        segments.push({type: "transition", data: [line_data]});
+        segments.push({type: "transition", data: []});
         e2v_std_min = line_data["smoothed2_future_kalman_acceleration_std"];
         first_e2v_value_for_transition = line_data["smoothed2_future_kalman_acceleration_std"];
+        tmp_buffer = [line_data];
+        index = 0;
+        check_for_escape = false;
+        com_thrust = thrust;
     }
     else { // check for the end of the transition
         const latest_segment_index = segments.length - 1;
         const latest_segment = segments[latest_segment_index];
         if (latest_segment.type === "steady") {
+            console.log("here steady continue");
             latest_segment.data.push(line_data);
         }
         else if (latest_segment.type === "transition") {
+            console.log("here transition continue");
             /*
             Here we need some careful logic...
             We need to travel down the first hump finding the first minimum value as we go.
@@ -136,20 +159,102 @@ data_lines.forEach((line) => {
             new idea do this in two rounds use the technique above to find a "provisional" steady state region.
             Then find the mean and stdev of the steady state segment and then create some threshold so when you are within 
             say one sigma you are included. the expand the steady state region based on the condition.
+            Could just do max and min for this region as bounds too.
             */
 
             // append this data to the output data so we can plot these transitions as a sanity check.
 
             // see if we have ended the transition
+
+            index++;
             const e2v_std = line_data["smoothed2_future_kalman_acceleration_std"];
             if (e2v_std < e2v_std_min ) {
-
+                console.log("New transition minumum discovered");
+                // we have a new minimum
                 e2v_std_min = e2v_std;
+                min_index = index;
+            }
+            
+            tmp_buffer.push(line_data);
+
+            if (check_for_escape && (e2v_std > (first_e2v_value_for_transition/2))) {
+                console.log("ESCAPE transition", e2v_std, first_e2v_value_for_transition/2);
+                // escape 
+                // take buffer from start to where min_index is.... this defines the transition segment
+                // the remaining section of the buffer is steady state
+
+                // buffer until min_index
+                const transition_data = tmp_buffer.slice(0, min_index);
+
+                // buffer after min_index
+                const steady_state_data = tmp_buffer.slice(min_index); // to end
+
+                latest_segment.data = transition_data;
+                segments.push({type:"steady", data:steady_state_data});
+            }
+
+            if (e2v_std < (first_e2v_value_for_transition/2)) {
+                check_for_escape = true;
             }
         }
     }
 });
 
+// there is the remaining transition / steady state data as the escape condition does not work for the last transition
+/*const latest_segment_index = segments.length - 1;
+const latest_segment = segments[latest_segment_index];
+
+// buffer up to min_index
+const transition_data = tmp_buffer.slice(0, min_index);
+
+// buffer after min_index
+const steady_state_data = tmp_buffer.slice(min_index); // to end
+latest_segment.data = transition_data;
+segments.push({type:"steady", data:[steady_state_data]});*/
+
+
+console.log("segments", segments);
+console.log("tmp_buffer", tmp_buffer);
+console.log("highest_velocity", highest_velocity);
+
+// ok so now rebuild data and when we are exactly at a transition then set a transition value of maximum velocity
+let output_lines = [];
+
+segments.forEach((segment) => {
+    const type = segment.type;
+    if (type === "steady") {
+        output_lines = output_lines.concat(segment.data);
+    }
+    else {
+        // console.log("transition non steady");
+        // we have a transition period
+        if (segment.data.length) {
+            segment.data[0]["transition_p"] = highest_velocity;
+            output_lines = output_lines.concat(segment.data);
+        }
+        // console.log("segment.data[0]", segment.data[0]);
+    }
+});
+
+output_lines.forEach((line) => {
+    if (!line.hasOwnProperty("transition_p")) {
+        line["transition_p"] = 0;
+    }
+})
+
+// console.log("output_lines", JSON.stringify(output_lines));
+
+const out_config = [...file_input_config_json.inputs, {"name": "transition_p", "position": 39}];
+
+console.log("out_config", out_config);
+
+const serialiser = new ASCIIParser(out_config, ",");
+
+const output_str = output_lines.map(line=>serialiser.serialise(line)).join("\n");
+
+console.log("output_str", output_str);
+
+fs.writeFileSync(full_output_data_path, output_str);
 
 
 /*
