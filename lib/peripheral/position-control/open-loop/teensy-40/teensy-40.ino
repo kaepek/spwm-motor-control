95709f1797ce37abe0e7f330f7275b7009ec54ff
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2023-11-09 18:06:03 +0000

    pos control formatting

--- lib/peripheral/position-control/open-loop/teensy-40/teensy-40.ino
+++ lib/peripheral/position-control/open-loop/teensy-40/teensy-40.ino
@@ -18,19 +18,16 @@ namespace kaepek
   class PositionControlL6234Teensy40 : public SerialInputControl<4>
   {
   private:
-    volatile int ELECTRICAL_DEG_PHASE_A = 0;
-    volatile int ELECTRICAL_DEG_PHASE_B = 0;
-    volatile int ELECTRICAL_DEG_PHASE_C = 0;
-    volatile int SPWM_DUTY_PHASE_A = 0;
-    volatile int SPWM_DUTY_PHASE_B = 0;
-    volatile int SPWM_DUTY_PHASE_C = 0;
-
-    volatile float TORQUE_VALUE = 0.0;
-    volatile signed int PHASE_CHANGE_VALUE = 0;
-
+    volatile int electrical_deg_phase_a = 0;
+    volatile int electrical_deg_phase_b = 0;
+    volatile int electrical_deg_phase_c = 0;
+    volatile int spwm_duty_phase_a = 0;
+    volatile int spwm_duty_phase_b = 0;
+    volatile int spwm_duty_phase_c = 0;
+    volatile float torque_value = 0.0;
+    volatile signed int phase_change_value = 0;
     volatile float total_time = 0.0;
-    volatile float deltaTime;
-
+    volatile float delta_time;
     bool enabled = false;
 
     // Logging timer interval.
@@ -56,10 +53,10 @@ namespace kaepek
         analogWrite(PIN_L6234_SPWM_PHASE_A, 0);
         analogWrite(PIN_L6234_SPWM_PHASE_B, 0);
         analogWrite(PIN_L6234_SPWM_PHASE_C, 0);
-                logging_timer.begin([this]
-                           { this->log_data(); },
-                           this->LOGGING_MICROS, true);
-                           
+        logging_timer.begin([this]
+                            { this->log_data(); },
+                            this->LOGGING_MICROS, true);
+
         break;
       case SerialInputCommandWord::Stop:
         enabled = false;
@@ -73,14 +70,14 @@ namespace kaepek
         break;
       case SerialInputCommandWord::Torque1UI16:
         com_torque_value = (data_buffer[1] << 8) | data_buffer[0];       // 0 -> 65535 // we want 0 -> 1
-        TORQUE_VALUE = min((float)com_torque_value / (float)65535, 0.7); // cap max to 0.7 ratio 70%.
+        torque_value = min((float)com_torque_value / (float)65535, 0.7); // cap max to 0.7 ratio 70%.
         break;
       case SerialInputCommandWord::Offset1F32:
         *((unsigned char *)&float_value + 0) = data_buffer[0];
         *((unsigned char *)&float_value + 1) = data_buffer[1];
         *((unsigned char *)&float_value + 2) = data_buffer[2];
         *((unsigned char *)&float_value + 3) = data_buffer[3];
-        PHASE_CHANGE_VALUE = (signed int)float_value / 100.0;
+        phase_change_value = (signed int)float_value / 100.0;
         break;
       default:
         Serial.println(control_word);
@@ -91,13 +88,13 @@ namespace kaepek
     void update_phase()
     {
       // increment rotation
-      ELECTRICAL_DEG_PHASE_A = ELECTRICAL_DEG_PHASE_A + PHASE_CHANGE_VALUE;
-      ELECTRICAL_DEG_PHASE_B = ELECTRICAL_DEG_PHASE_A + 120;
-      ELECTRICAL_DEG_PHASE_C = ELECTRICAL_DEG_PHASE_B + 120;
+      electrical_deg_phase_a = electrical_deg_phase_a + phase_change_value;
+      electrical_deg_phase_b = electrical_deg_phase_a + 120;
+      electrical_deg_phase_c = electrical_deg_phase_b + 120;
       // obey modular arithmetic (loop at 360)
-      ELECTRICAL_DEG_PHASE_A %= 360;
-      ELECTRICAL_DEG_PHASE_B %= 360;
-      ELECTRICAL_DEG_PHASE_C %= 360;
+      electrical_deg_phase_a %= 360;
+      electrical_deg_phase_b %= 360;
+      electrical_deg_phase_c %= 360;
     }
 
     float deg_to_rad(int16_t electrical_deg_phase_x)
@@ -123,20 +120,20 @@ namespace kaepek
       if (enabled == false)
         return;
       // timing diagnostics
-      long int oldTime = t1;
+      long int old_time = t1;
       t1 = millis();
-      deltaTime = (t1 - oldTime);
-      total_time += (float)deltaTime / (float)1000;
+      delta_time = (t1 - old_time);
+      total_time += (float)delta_time / (float)1000;
 
       // calculate SPWM duties
-      SPWM_DUTY_PHASE_A = (int)(spwm_duty_calculator(ELECTRICAL_DEG_PHASE_A) * TORQUE_VALUE);
-      SPWM_DUTY_PHASE_B = (int)(spwm_duty_calculator(ELECTRICAL_DEG_PHASE_B) * TORQUE_VALUE);
-      SPWM_DUTY_PHASE_C = (int)(spwm_duty_calculator(ELECTRICAL_DEG_PHASE_C) * TORQUE_VALUE);
+      spwm_duty_phase_a = (int)(spwm_duty_calculator(electrical_deg_phase_a) * torque_value);
+      spwm_duty_phase_b = (int)(spwm_duty_calculator(electrical_deg_phase_b) * torque_value);
+      spwm_duty_phase_c = (int)(spwm_duty_calculator(electrical_deg_phase_c) * torque_value);
 
       // update PWM duties
-      analogWrite(PIN_L6234_SPWM_PHASE_A, SPWM_DUTY_PHASE_A);
-      analogWrite(PIN_L6234_SPWM_PHASE_B, SPWM_DUTY_PHASE_B);
-      analogWrite(PIN_L6234_SPWM_PHASE_C, SPWM_DUTY_PHASE_C);
+      analogWrite(PIN_L6234_SPWM_PHASE_A, spwm_duty_phase_a);
+      analogWrite(PIN_L6234_SPWM_PHASE_B, spwm_duty_phase_b);
+      analogWrite(PIN_L6234_SPWM_PHASE_C, spwm_duty_phase_c);
 
       delayMicroseconds(1000);
     }
@@ -200,20 +197,20 @@ namespace kaepek
     {
       cli();
       // diagnostic prints
-      Serial.print(PHASE_CHANGE_VALUE);
+      Serial.print(phase_change_value);
       Serial.print(",");
-      Serial.print(ELECTRICAL_DEG_PHASE_A);
+      Serial.print(electrical_deg_phase_a);
       Serial.print(",");
-      Serial.print(TORQUE_VALUE);
+      Serial.print(torque_value);
       Serial.print(",");
-      Serial.print(deltaTime, 4);
+      Serial.print(delta_time, 4);
 
       Serial.print(",");
-      Serial.print(SPWM_DUTY_PHASE_A);
+      Serial.print(spwm_duty_phase_a);
       Serial.print(",");
-      Serial.print(SPWM_DUTY_PHASE_B);
+      Serial.print(spwm_duty_phase_b);
       Serial.print(",");
-      Serial.print(SPWM_DUTY_PHASE_C);
+      Serial.print(spwm_duty_phase_c);
 
       Serial.print(",");
       Serial.print(total_time, 4);
